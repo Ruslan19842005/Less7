e9cb1fcfd38eb14ccb59c79294bb05810c1e4fd9
yamal <yamal_07@mail.ru>
2025-05-08 17:22:23 +0900

    Номер 2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ public class Main {
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
+        }
+
+            for (int i = 10;  i >= 1; i--) {
+                System.out.println(i);
+            }
 
 
 
         }
     }
-}
+
 
 
 
